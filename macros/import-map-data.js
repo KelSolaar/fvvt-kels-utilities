a442337e724ea3d2b42d05a713bb33f898b063e6
Thomas Mansencal <thomas.mansencal@gmail.com>
2023-02-22 21:42:11 +1300

    Implement support for "Faerûn" map data import.

--- macros/import-map-data.js
+++ macros/import-map-data.js
@@ -3,15 +3,31 @@ Import the map data from a json file generated with the following python
 script: https://gist.github.com/KelSolaar/6f0847adb6bfa08665287a18039c8e24
 */
 
+/*
 const mapName = "Waterdeep";
 const offset = [1144, 1169];
 const scale = [0.9795, 0.9795];
+*/
+
+const mapName = "Faerûn";
+const offset = [202, 205];
+const scale = [1.5243, 1.5247];
+
 const icons = {
+  Undefined: {
+    icon: "modules/kels-utilities/icons/circle.svg",
+    tint: "#ffffff",
+  },
   Area: { icon: "modules/kels-utilities/icons/circle.svg", tint: "#ffffff" },
   Business: { icon: "icons/svg/hanging-sign.svg", tint: "#f44336" },
   "City Building": {
     icon: "modules/kels-utilities/icons/building.svg",
     tint: "#9c27b0",
+  },
+  Forest: { icon: "modules/kels-utilities/icons/forest.svg", tint: "#8bc34a" },
+  "Inn, Tavern, Festhall": {
+    icon: "modules/kels-utilities/icons/beer.svg",
+    tint: "#03a9f4",
   },
   Guildhall: { icon: "modules/kels-utilities/icons/hall.svg", tint: "#3f51b5" },
   "Inn, Tavern, Festhall": {
@@ -22,17 +38,36 @@ const icons = {
     icon: "modules/kels-utilities/icons/miscellaneous.svg",
     tint: "#009688",
   },
+  Mountainous: {
+    icon: "icons/svg/mountain.svg",
+    tint: "#e3e8eb",
+  },
   "Noble Villa": { icon: "icons/svg/house.svg", tint: "#8bc34a" },
+  Place: {
+    icon: "icons/svg/village.svg",
+    tint: "#f44336",
+  },
+  Road: {
+    icon: "modules/kels-utilities/icons/circle.svg",
+    tint: "#ffffff",
+  },
   Temple: { icon: "icons/svg/temple.svg", tint: "#ffeb3b" },
   Warehouse: {
     icon: "modules/kels-utilities/icons/warehouse.svg",
     tint: "#ff9800",
   },
+  Water: {
+    icon: "modules/kels-utilities/icons/water.svg",
+    tint: "#03a9f4",
+  },
 };
 
-const mapData = await fetch(`atlas/${mapName.toLowerCase()}_data.json`).then(
-  (response) => response.json()
-);
+const mapData = await fetch(
+  `atlas/${mapName
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/\p{Diacritic}/gu, "")}_data.json`
+).then((response) => response.json());
 
 let folder = game.folders.contents.find(
   (folder) => folder.type === "JournalEntry" && folder.name === mapName
@@ -84,13 +119,18 @@ for (const location of mapData.locations) {
     { parent: journalEntry, displaySheet: false }
   );
 
+  let type = location.type;
+  if (!type) {
+    type = "Undefined";
+  }
+
   const noteData = {
     entryId: journalEntry.id,
     text: location.name,
     global: false,
     texture: {
-      src: icons[location.type].icon,
-      tint: icons[location.type].tint,
+      src: icons[type].icon,
+      tint: icons[type].tint,
     },
     iconSize: 32,
     x: location.coordinates[0] * scale[0] + offset[0],
